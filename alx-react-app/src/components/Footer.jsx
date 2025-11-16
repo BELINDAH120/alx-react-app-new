6072992f8a0a4af0221658026e7d351c14d277ed
BELINDAH120 <128031203+BELINDAH120@users.noreply.github.com>
2025-11-16 12:51:19 +0100

    Add inline styles to Footer component
    Updated Footer component to include inline styles.

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,6 +1,15 @@
+// src/components/Footer.jsx
 function Footer() {
   return (
-    <footer>
+    <footer
+      style={{
+        backgroundColor: "#222",
+        color: "white",
+        textAlign: "center",
+        padding: "15px",
+        marginTop: "20px",
+      }}
+    >
       <p>© 2023 City Lovers</p>
     </footer>
   );
